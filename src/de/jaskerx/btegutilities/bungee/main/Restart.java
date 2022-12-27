e40462b177eb2480d64f119a8b7faed0d5796949
JaskerX <jaskerlp@gmx.de>
2022-12-27 16:19:18 +0100

    updated chat + action bar infos

--- src/de/jaskerx/btegutilities/bungee/main/Restart.java
+++ src/de/jaskerx/btegutilities/bungee/main/Restart.java
@@ -11,6 +11,7 @@ import javax.annotation.Nullable;
 
 import net.md_5.bungee.BungeeCord;
 import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.TextComponent;
@@ -24,7 +25,6 @@ public class Restart {
 	Map<String, ServerInfo> restartedServers = new TreeMap<>();
 	boolean timerRuns = false;
 	int counter = 120;
-	int[] specialTimes = new int[] {1800, 900, 600, 300, 120, 60};
 	Timer timer;
 	boolean whenEmpty;
 	private static boolean doProxyRestart = true;
@@ -69,32 +69,48 @@ public class Restart {
 				@Override
 				public void run() {
 					
-					sendMessage(ChatMessageType.ACTION_BAR, "§c§lServerrestart §c§lin §c§l" + counter);
-					
-					for(int t : specialTimes) {
-						if(counter == t) {
-							if(counter == 60 ) {
-								sendMessage(ChatMessageType.CHAT, "§b§lBTEG §7» §cServerrestart §cin §11 §1minute" + "!");
-							} else {
-								sendMessage(ChatMessageType.CHAT, "§b§lBTEG §7» §cServerrestart §cin §1" + counter/60 + " §1minutes" + "!");
-							}
-						}
-					}
-					
-					if(counter == 30 || counter <= 15) {
-						if(counter == 1) {
-							sendMessage(ChatMessageType.CHAT, "§b§lBTEG §7» §cServerrestart §cin §11 §1second!");
-						} else {
-							sendMessage(ChatMessageType.CHAT, "§b§lBTEG §7» §cServerrestart §cin §1" + counter + " §1seconds!");
-						}
-					}
-					
 					if(counter == 0) {
 						restart();
 						timerRuns = false;
 						counter = 120;
 						servers.clear();
 						BungeeCord.getInstance().getScheduler().cancel(Main.getInstance());
+						return;
+					}
+					
+					int h = counter / 60 / 60;
+					int min = (counter / 60) % 60;
+					int s = counter % 60;
+					
+					String hours = "§1hour" + (h != 1 ? "s" : "");
+					String minutes = "§1" + min + " §1minute" + (min != 1 ? "s" : "");
+					String seconds = "§1" + s + " §1second" + (s != 1 ? "s" : "");
+										
+					if(counter == delay) {
+						if(p != null) p.sendMessage(new ComponentBuilder("§b§lBTEG §7» §6Restarting §6in §9" + h + ":" + min + ":" + s + " §h").create());
+					}
+					
+					// hours
+					if(min == 0 && s == 0) {
+						sendMessage(ChatMessageType.CHAT, "§b§lBTEG §7» §cServerrestart §cin §1" + hours + "!");
+					}
+					
+					// last hour
+					if(h == 0) {
+						
+						// chat	minutes						
+						if((s == 0 && (min == 30 || min == 15 || min == 10 || min == 5 || min == 1)) || (s == 30 && min == 1)) {
+							sendMessage(ChatMessageType.CHAT, "§b§lBTEG §7» §cServerrestart §cin " + minutes + (s != 0 ? " §1and §1" + seconds : "") + "!");
+						}
+						// chat seconds last minute
+						if(min == 0 && (s == 30 || s <= 15)) {
+							sendMessage(ChatMessageType.CHAT, "§b§lBTEG §7» §cServerrestart §cin " + seconds + "!");
+						}
+						
+						// action bar
+						if(min < 15) {
+							sendMessage(ChatMessageType.ACTION_BAR, "§c§lServerrestart §c§lin §1" + (min >= 1 ? (s >= 1 ? min + ":" + (s < 10 ? "0" : "") + s + " §1min" : minutes) : seconds));
+						}						
 					}
 					
 					counter--;
