d3354c4d156d5febfa74353d9c26778839802de2
JaskerX <jaskerlp@gmx.de>
2025-01-19 17:10:41 +0100

    improved /plots

--- src/main/java/de/jaskerx/bteg/utilities/bungee/commands/PlotsCommand.java
+++ src/main/java/de/jaskerx/bteg/utilities/bungee/commands/PlotsCommand.java
@@ -2,17 +2,32 @@ package de.jaskerx.bteg.utilities.bungee.commands;
 
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class PlotsCommand extends Command {
 
     public PlotsCommand() {
-        super("plots");
+        super("plotsystem");
     }
 
     @Override
     public void execute(CommandSender commandSender, String[] strings) {
-        ProxyServer.getInstance().getPluginManager().dispatchCommand(commandSender, "server Plot-1");
+        if (!(commandSender instanceof ProxiedPlayer player)) {
+            commandSender.sendMessage(new ComponentBuilder("ᾠ §cDiesen §cCommand §ckönnen §cnur §cSpieler §cnutzen.").create());
+            return;
+        }
+
+        ServerInfo serverInfo = ProxyServer.getInstance().getServerInfo("Plot-1");
+
+        if (serverInfo == null) {
+            player.sendMessage(new ComponentBuilder("ᾠ §cDer §cPlotserver §cist §cleider §cnicht §cverfügbar.").create());
+            return;
+        }
+
+        player.connect(serverInfo);
     }
 
 }
